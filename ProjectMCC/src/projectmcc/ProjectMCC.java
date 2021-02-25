9ee36178f977a982c8fd6446e70ffe53648964ef
gabri <gabri@LAPTOP-LN5SEGUU>
2021-02-25 15:17:06 +0700

    ProjectMCC.v2-Gabriel

--- ProjectMCC/src/projectmcc/ProjectMCC.java
+++ ProjectMCC/src/projectmcc/ProjectMCC.java
@@ -16,6 +16,7 @@ public class ProjectMCC {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Gabriel Bintang Timur");
     }
     
 }
