a208425827326410e7d2754ccc4c65ea7ef97452
gabri <gabri@LAPTOP-LN5SEGUU>
2021-02-26 16:52:53 +0700

    projectSpring

---
+++ hrdb/src/main/java/hrdb/hrdb/serviceimplements/employeeService.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hrdb.hrdb.serviceimplements;
+
+import hrdb.hrdb.entities.Employees;
+import hrdb.hrdb.repository.employeeRepository;
+import hrdb.hrdb.serviceinterface.employeeServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author gabri
+ */
+@Service
+public class employeeService implements employeeServiceInterface {
+
+    @Autowired
+    private employeeRepository r;
+
+    @Override
+    public Iterable<Employees> getAll() {
+        return r.findAll();
+    }
+
+}
